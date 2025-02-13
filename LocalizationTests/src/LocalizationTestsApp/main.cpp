53ef8e0d2eb68e636ee5a8648abd5a11963994bd
Dewei Chen <deweiche@ucm.es>
2025-02-13 17:29:49 +0100

    Limpieza inicial de archivos hechos por Chema.

--- LocalizationTests/src/LocalizationTestsApp/main.cpp
+++ LocalizationTests/src/LocalizationTestsApp/main.cpp
@@ -1,12 +1,8 @@
 ﻿#include <iostream>
 #include <string>
 #include "Tesseract.h"
-#include "BadlyImplemented.h"
 #include "LocalizationTests.h"
 #include "Overlap.h"
-#include "FontIssue.h"
-#include "DictionaryManager.h"
-#include "OrthographicError.h"
 
 #include <spdlog/spdlog.h>
 #include "spdlog/sinks/stdout_color_sinks.h"
@@ -21,8 +17,7 @@ using json = nlohmann::json;
 
 int main(int argc, char *argv[]) {
 
-	/*FontIssue f = FontIssue("¿Hola qué tal?");
-	f.test("\ufffdHola qu\ufffd tal?");*/
+
 
 	
 	spdlog::info("Welcome to spdlog!");
@@ -57,13 +52,6 @@ int main(int argc, char *argv[]) {
 	test->run();
 	test->release();
 	delete test;
-
-	DictionaryManager dictMan;
-	if (dictMan.setup())
-		std::cout << "Dictionaries found" << std::endl;
-
-	OrtographicError o = OrtographicError(dictMan.getDict(DictionaryManager::Language::Spanish));
-	o.test("Hoala soy yo");
 
 	std::string volumen = "/home/localizationtests/volumen/";
 	OCR* ocr = new Tesseract();
